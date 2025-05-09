e1da25f53fe2e223b477fdb787e3140ab73df9a9
sxbe <sabeeh.sheikh03@gmail.com>
2025-05-09 15:17:46 -0400

    feat: base terraform scaffold

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"   # change to ca-central-1 or another region if you like
+}
